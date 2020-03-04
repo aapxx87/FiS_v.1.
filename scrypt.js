53a38bb0902cf333457a496781e8d3337d839af4
aapspb1987 <aapspb1987@gmail.com>
2020-03-04 21:49:39 +0300

    Html add доп функционал

--- scrypt.js
+++ scrypt.js
@@ -2,19 +2,19 @@ const findFarm = (farm) => {
 
  if (farm === 'Амелотекс' || farm === 'Гриппферон' || farm === 'Дифлюкан' || farm === 'Зовиракс' || farm === 'Лидокаин спрей' || farm === 'Макмирор' || farm === 'Мидокалм' || farm === 'Микролакс' || farm === 'Млекоин' || farm === 'Найз табл' || farm === 'Но-шпа внутривенно' || farm === 'Но-шпа таблетки' || farm === 'Нурофен экспресс' || farm === 'Нурофен сироп детский' || farm === 'Отофа' || farm === 'Офтагель' || farm === 'Парацетомол' || farm === 'Реаферон-Липинт' || farm === 'Burn free' || farm === 'Hydrocortison' || farm === 'Пластырь детский' || farm === 'Терафлю') {
  return 'Коробка 1';
- } 
-    
-    
+ }
+
+
  if (farm === 'Таваник' || farm === 'Тева мазь') {
  return 'Коробка 2';
- } 
-  
-  
+ }
+
+
  if (farm === 'Регидрон' || farm === 'Самоклеящийся бинт') {
  return 'Коробка 3';
- } 
-  
-  
+ }
+
+
  else {
    return 'Данного продукта нет'
  }
@@ -25,10 +25,32 @@ const findFarm = (farm) => {
 function poisk(){
   let str;
   let result;
-  
+
   str = document.getElementById('inp_1').value;
-  
+
   result = findFarm(str);
-  
+
   document.getElementById('result1').innerHTML = result;
 };
+
+
+
+const kont1 = ['Амелотекс', 'Гриппферон', 'Дифлюкан', 'Зовиракс', 'Лидокаин спрей', 'Макмирор', 'Мидокалм', 'Микролакс', 'Млекоин', 'Найз табл', 'Но-шпа внутривенно', 'Но-шпа таблетки', 'Нурофен экспресс'];
+
+function addmassiv(){
+    document.getElementById('rezult').innerHTML = kont1.join(", ");
+    };
+
+
+const kont2 = ['Таваник', 'Тева мазь'];
+
+function addmassiv1(){
+    document.getElementById('rezult').innerHTML = kont2.join(", ");
+    };
+
+
+const kont3 = ['Регидрон', 'Самоклеящийся бинт'];
+
+function addmassiv2(){
+    document.getElementById('rezult').innerHTML = kont3.join(", ");
+    };
