8e747076079fa9ae3ced0bf7e20d9d72e45b1a03
Anatolij <aapxx87@gmail.com>
2020-03-06 08:22:59 +0300

    V. 1.1 (update scrypt.js)
    Добавил возможность проверки значения в инпуте по регистру первой буквы. То есть теперь можно вводить название лекарства и с маленьклой и с большой буквы при поиске.

--- scrypt.js
+++ scrypt.js
@@ -1,3 +1,23 @@
+
+const bigBukwa = (str) => {
+
+if (str[0] ===  str[0].toUpperCase()) {
+
+  return str;
+}
+
+if (str[0] !== str[0].toUpperCase()) {
+
+   const newString = str[0];
+   const newString2 = str[0].toUpperCase();
+   const body = str.slice(1);
+   const newString3 = newString2 + body;
+   return newString3;
+
+}
+
+};
+
 const findFarm = (farm) => {
 
  if (farm === 'Амелотекс' || farm === 'Гриппферон' || farm === 'Дифлюкан' || farm === 'Зовиракс' || farm === 'Лидокаин спрей' || farm === 'Макмирор' || farm === 'Мидокалм' || farm === 'Микролакс' || farm === 'Млекоин' || farm === 'Найз табл' || farm === 'Но-шпа внутривенно' || farm === 'Но-шпа таблетки' || farm === 'Нурофен экспресс' || farm === 'Нурофен сироп детский' || farm === 'Отофа' || farm === 'Офтагель' || farm === 'Парацетомол' || farm === 'Реаферон-Липинт' || farm === 'Burn free' || farm === 'Hydrocortison' || farm === 'Пластырь детский' || farm === 'Терафлю' || farm === 'Пластырь детский' || farm === 'Пластырь детский' || farm === 'Зостерин-ультра' || farm === 'Ринофлуимуцил' || farm === 'Тантум-Верде спрей' || farm === 'Доктор мом' || farm === 'Панатус форте' || farm === 'Отикапс') {
@@ -28,7 +48,7 @@ function poisk(){
 
   str = document.getElementById('inp_1').value;
 
-  result = findFarm(str);
+  result = findFarm(bigBukwa(str));
 
   document.getElementById('result1').innerHTML = result;
 };
